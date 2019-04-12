8fccc45a9b7c7291a4619c25e520b963ea182935
eyetpn <eyetpn@gmail.com>
2019-04-11 22:26:32 -0600

    Update num_list.py

--- num_list.py
+++ num_list.py
@@ -2,8 +2,8 @@
 nums = [12, 10, 32, 3, 66, 17, 42, 99, 20]
 
 def print_each_number():   
-for i in nums():
-    print (i)
+    for i in nums():
+        print (i)
 def print_each_number_and_its_square():
-for i in nums():
-    print ("The square of ", i, "is ", i**2)
+    for i in nums():
+        print ("The square of ", i, "is ", i**2)
